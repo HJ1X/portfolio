9acf6f961b69587a053ee1ee6c6d2de239afe2e3
Arpit Das <arpitdas118@gmail.com>
2024-02-08 23:39:58 +0530

    created grid structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Grid, GridItem } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
 function App() {
-  return <></>;
+  return (
+    <Grid templateAreas={`"nav main"`} gridTemplateColumns={"180px 1fr"}>
+      <GridItem area="nav">
+        <Navbar />
+      </GridItem>
+      <GridItem area="main">
+        <Main />
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
