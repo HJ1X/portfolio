937bdd879807b93a6ae35736ad3ea3f1c661ccc4
Arpit Das <arpitdas118@gmail.com>
2024-02-12 23:41:20 +0530

    created icons component and added react-icons dependency

---
+++ src/components/IconList.tsx
@@ -0,0 +1,35 @@
+import { HStack, Icon, Link, useColorMode } from "@chakra-ui/react";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+const IconList = () => {
+  const { colorMode } = useColorMode();
+
+  const icons = [
+    {
+      icon: FaLinkedinIn,
+      link: "https://in.linkedin.com/",
+    },
+    {
+      icon: FaGithub,
+      link: "https://github.com/",
+    },
+  ];
+
+  return (
+    <HStack spacing={4}>
+      {icons.map(({ icon, link }) => (
+        <Link href={link} isExternal>
+          <Icon
+            as={icon}
+            key={icon.toString()}
+            // needs to be updated as per theme
+            color={colorMode === "light" ? "white" : "grey"}
+            fontSize="2xl"
+          ></Icon>
+        </Link>
+      ))}
+    </HStack>
+  );
+};
+
+export default IconList;
