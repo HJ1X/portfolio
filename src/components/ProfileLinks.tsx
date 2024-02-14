26fca4a625e4a64059ebd433eeab49f25e91481e
Himanshu Jaroli <54548986+HJ1X@users.noreply.github.com>
2024-02-15 00:30:45 +0530

    add profile links to layout

--- src/components/ProfileLinks.tsx
+++ src/components/ProfileLinks.tsx
@@ -1,22 +1,26 @@
 import { HStack, Icon, Link, useColorMode } from "@chakra-ui/react";
-import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 const ProfileLinks = () => {
   const { colorMode } = useColorMode();
 
   const icons = [
     {
+      icon: FaGithub,
+      link: "https://github.com/",
+    },
+    {
       icon: FaLinkedinIn,
       link: "https://in.linkedin.com/",
     },
     {
-      icon: FaGithub,
-      link: "https://github.com/",
+      icon: FaTwitter,
+      link: "https://twitter.com",
     },
   ];
 
   return (
-    <HStack spacing={4}>
+    <HStack spacing={4} pl={4}>
       {icons.map(({ icon, link }) => (
         <Link href={link} isExternal>
           <Icon
