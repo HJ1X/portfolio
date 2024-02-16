190aa7c34d5b61c99bdb483c8192900a9d015cb0
Himanshu Jaroli <54548986+HJ1X@users.noreply.github.com>
2024-02-17 02:09:48 +0530

    change section item attached card style from pseudo elements to box elements

--- src/components/SectionItem.tsx
+++ src/components/SectionItem.tsx
@@ -1,50 +1,19 @@
-import { Box, Card, theme } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { Section } from "../data/sections";
+import CanvasAttachedCard from "./CanvasAttachedCard";
 
-interface NavbarItemProps {
+interface SectionItem {
   isActive: boolean;
   section: Section;
   onClick: () => void;
 }
 
-const getPseudoElementStyles = (elementType: "before" | "after") => {
-  const verticalPlacement = elementType === "before" ? "bottom" : "top";
-  const boxShadowVeticalOffset =
-    elementType === "before" ? "-0.5rem" : "0.5rem";
-
-  return {
-    position: "absolute",
-    content: "' '",
-    [verticalPlacement]: "-1rem",
-    right: 0,
-    width: "1rem",
-    height: "1rem",
-    borderRadius: "50%",
-    boxShadow: `0.5rem ${boxShadowVeticalOffset} ${theme.colors.white}`,
-  };
-};
-
-const SectionItem = ({ isActive, section, onClick }: NavbarItemProps) => {
+const SectionItem = ({ isActive, section, onClick }: SectionItem) => {
   return (
     <Box onClick={onClick} cursor="pointer">
-      {isActive ? (
-        <Card
-          zIndex="4"
-          _before={getPseudoElementStyles("before")}
-          _after={getPseudoElementStyles("after")}
-          borderLeftRadius="xl"
-          // TODO: Update box-shadow if required
-          boxShadow="0 25px 50px -12px rgba(0, 0, 0, 0.25)"
-          clipPath="inset(-50px 0px -50px -50px)"
-          p={4}
-        >
-          {section.name}
-        </Card>
-      ) : (
-        <Card backgroundColor="transparent" color="white" boxShadow="" p={4}>
-          {section.name}
-        </Card>
-      )}
+      <CanvasAttachedCard isActive={isActive}>
+        {section.name}
+      </CanvasAttachedCard>
     </Box>
   );
 };
