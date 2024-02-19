e500494ecc30c10b1be0ed922fc5fc1ab8491778
Abhimaanyu Vashishth <abhi.vashishth99@gmail.com>
2024-02-20 00:24:16 +0530

    changed layout

--- src/consts.ts
+++ src/consts.ts
@@ -9,6 +9,7 @@ export const DARK_MODE_CARD_BG = theme.colors.gray[700];
 export const DARK_MODE_SUBTLE_BG = theme.colors.gray[700];
 export const DARK_MODE_SUBTLE_TEXT = theme.colors.gray[400];
 export const DARK_MODE_PLACEHOLDER_COLOR = theme.colors.whiteAlpha[400];
+export const DARK_MODE_BUTTON_COLOR = theme.colors.yellow[300];
 
 export const LIGHT_MODE_DEFAULT_TEXT = theme.colors.gray[800];
 export const LIGHT_MODE_DEFAULT_BG = theme.colors.white;
@@ -16,6 +17,7 @@ export const LIGHT_MODE_CARD_BG = LIGHT_MODE_DEFAULT_BG;
 export const LIGHT_MODE_SUBTLE_BG = theme.colors.gray[100];
 export const LIGHT_MODE_SUBTLE_TEXT = theme.colors.gray[600];
 export const LIGHT_MODE_PLACEHOLDER_COLOR = theme.colors.gray[500];
+export const LIGHT_MODE_BUTTON_COLOR = theme.colors.gray[800];
 
 export const TRANSITION_DEFAULT = "all 100ms ease-out";
 export const TRANSITION_SLOWER = "all 150ms ease-out";
