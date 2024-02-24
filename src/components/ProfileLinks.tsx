2bc9e41b21d01c32faabc63d8a0d264c7342b942
Abhimaanyu Vashishth <abhi.vashishth99@gmail.com>
2024-02-24 23:13:55 +0530

    reverting the link hover efffect

--- src/components/ProfileLinks.tsx
+++ src/components/ProfileLinks.tsx
@@ -1,10 +1,6 @@
 import { Stack, Icon, Link, useColorModeValue } from "@chakra-ui/react";
 import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
-import {
-  DARK_MODE_DEFAULT_BG,
-  LIGHT_MODE_DEFAULT_BG,
-  TRANSITION_DEFAULT,
-} from "../consts";
+import { DARK_MODE_DEFAULT_BG, LIGHT_MODE_DEFAULT_BG } from "../consts";
 
 const ProfileLinks = () => {
   const iconColor = useColorModeValue(
@@ -31,13 +27,7 @@ const ProfileLinks = () => {
     <Stack spacing={4}>
       {icons.map(({ icon, link }) => (
         <Link href={link} isExternal key={link}>
-          <Icon
-            as={icon}
-            color={iconColor}
-            fontSize="xl"
-            _hover={{ fontSize: "2xl" }}
-            transition={TRANSITION_DEFAULT}
-          ></Icon>
+          <Icon as={icon} color={iconColor} fontSize="2xl"></Icon>
         </Link>
       ))}
     </Stack>
