cabece018fc1c2fa76f03daff904f18bb99875a4
Himanshu Jaroli <54548986+HJ1X@users.noreply.github.com>
2025-01-08 21:59:50 +0530

    update: setup theme for chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
 import { ColorModeProvider } from "@/components/ui/color-mode.tsx";
 import { Provider } from "@/components/ui/provider.tsx";
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { RouterProvider } from "react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import system from "./theme";
 import router from "./routes";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <ChakraProvider value={defaultSystem}>
+      <ChakraProvider value={system}>
         <ColorModeProvider>
           <RouterProvider router={router} />
         </ColorModeProvider>
---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+import { createSystem, defaultConfig, defineConfig } from "@chakra-ui/react";
+
+const config = defineConfig({
+  theme: {
+    tokens: {
+      colors: {
+        // theme based purple color code
+        purple: "purple",
+      },
+    },
+    semanticTokens: {
+      colors: {
+        primary: { value: "{colors.purple}" },
+      },
+    },
+  },
+});
+
+export default createSystem(defaultConfig, config);
