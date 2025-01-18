2f7ece638089d487d767b2f53e73001899ec14c6
Himanshu Jaroli <54548986+HJ1X@users.noreply.github.com>
2025-01-18 12:10:27 +0530

    feat: add fancy card component

---
+++ src/components/ui/custom/FancyCard.tsx
@@ -0,0 +1,60 @@
+import { Box, Card, CardRootProps } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+interface FancyCardProps extends CardRootProps {
+  children: ReactNode;
+  selected?: boolean;
+}
+
+const FancyCard = ({ selected = true, children, ...rest }: FancyCardProps) => {
+  return (
+    <Card.Root
+      aria-selected={selected}
+      transition="all"
+      position="relative"
+      _before={{
+        content: `""`,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: 0,
+        height: "full",
+        rounded: "md",
+        zIndex: "base",
+        bg: "gray.100",
+        transitionProperty: "all",
+        transitionDuration: "slow",
+        transitionTimingFunction: "ease-in",
+      }}
+      _after={{
+        content: `""`,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: 0,
+        height: "full",
+        rounded: "md",
+        zIndex: "base",
+        bg: "bg.primary",
+        transitionProperty: "all",
+        transitionDuration: "slow",
+        transitionTimingFunction: "ease-in",
+      }}
+      _hover={{
+        _before: {
+          width: selected ? 0 : "full",
+        },
+      }}
+      _selected={{
+        _after: {
+          width: "full",
+        },
+      }}
+      {...rest}
+    >
+      <Box zIndex="docked">{children}</Box>
+    </Card.Root>
+  );
+};
+
+export default FancyCard;
