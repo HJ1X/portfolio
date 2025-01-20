ed7d41f2c5c69cce2b9bafd21d27fdad42974913
Himanshu Jaroli <54548986+HJ1X@users.noreply.github.com>
2025-01-21 00:16:48 +0530

    update: add framer motion

---
+++ src/utils/animation.ts
@@ -0,0 +1,19 @@
+export const revealFromBottom = {
+  transform: ["translateY(1rem)", "translateY(0)"],
+  opacity: [0, 1],
+};
+
+export const revealFromTop = {
+  transform: ["translateY(-1rem)", "translateY(0)"],
+  opacity: [0, 1],
+};
+
+export const revealFromLeft = {
+  transform: ["translateX(-1rem)", "translateX(0)"],
+  opacity: [0, 1],
+};
+
+export const revealFromRight = {
+  transform: ["translateX(1rem)", "translateX(0)"],
+  opacity: [0, 1],
+};
