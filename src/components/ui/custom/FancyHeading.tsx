7fda9daae46f976c62bf6e1ffba5325c21119d26
Himanshu Jaroli <54548986+HJ1X@users.noreply.github.com>
2025-01-21 00:20:17 +0530

    update: add forward ref in fancy heading

--- src/components/ui/custom/FancyHeading.tsx
+++ src/components/ui/custom/FancyHeading.tsx
@@ -1,44 +1,47 @@
 import { Heading, HeadingProps } from "@chakra-ui/react";
+import { forwardRef, Ref } from "react";
 
 interface FancyHeadingProps extends HeadingProps {
   theme?: "primary" | "secondary";
 }
 
-const FancyHeading = ({
-  children,
-  theme = "primary",
-  ...rest
-}: FancyHeadingProps) => {
-  return (
-    <Heading
-      color={theme === "primary" ? "fg.primary" : "fg.secondary"}
-      position="relative"
-      overflow="hidden"
-      display="inline-block"
-      _before={{
-        content: `"${children as string}"`,
-        position: "absolute",
-        overflow: "hidden",
-        top: 0,
-        left: "0",
-        width: "0",
-        whiteSpace: "nowrap",
-        color:
-          theme === "primary" ? "fg.primary.darker" : "fg.secondary.darker",
-        transitionProperty: "all",
-        transitionDuration: "slow",
-        transitionTimingFunction: "ease-in",
-      }}
-      _hover={{
-        _before: {
-          width: "full",
-        },
-      }}
-      {...rest}
-    >
-      {children}
-    </Heading>
-  );
-};
+const FancyHeading = forwardRef(
+  (
+    { children, theme = "primary", ...rest }: FancyHeadingProps,
+    ref: Ref<HTMLHeadingElement>
+  ) => {
+    return (
+      <Heading
+        ref={ref}
+        color={theme === "primary" ? "fg.primary" : "fg.secondary"}
+        position="relative"
+        overflow="hidden"
+        display="inline-block"
+        _before={{
+          content: `"${children as string}"`,
+          position: "absolute",
+          overflow: "hidden",
+          top: 0,
+          left: "0",
+          width: "0",
+          whiteSpace: "nowrap",
+          color:
+            theme === "primary" ? "fg.primary.darker" : "fg.secondary.darker",
+          transitionProperty: "all",
+          transitionDuration: "slow",
+          transitionTimingFunction: "ease-in",
+        }}
+        _hover={{
+          _before: {
+            width: "full",
+          },
+        }}
+        {...rest}
+      >
+        {children}
+      </Heading>
+    );
+  }
+);
 
 export default FancyHeading;
