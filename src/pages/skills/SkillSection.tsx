2792ddcee01605f670cfb8d0917123a49649e7d3
Himanshu Jaroli <54548986+HJ1X@users.noreply.github.com>
2025-01-26 19:07:53 +0530

    feat: improv skill page UI

--- src/pages/skills/SkillSection.tsx
+++ src/pages/skills/SkillSection.tsx
@@ -1,44 +1,41 @@
-import { ProgressBar } from "@/components/ui/chakra/progress";
-import Badge from "@/components/ui/custom/Badge";
-import { Box, Flex, Heading, ProgressRoot } from "@chakra-ui/react";
-import { ReactNode } from "react";
+import Skill from "@/pages/skills/Skill";
+import { MotionBox, MotionHeading } from "@/components/ui/Motion";
+import { Skill as ISkill } from "@/data/skillsData";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { grow, revealFromBottom, revealFromLeft } from "@/utils/animation";
 
 interface SkillSectionProps {
-  data: {
-    label: string;
-    icon: ReactNode;
-    value: number;
-    skills: string[];
-  };
+  title: string;
+  description: string;
+  skills: ISkill[];
 }
 
-const SkillSection = ({ data }: SkillSectionProps) => {
+const SkillSection = ({ title, skills, description }: SkillSectionProps) => {
   return (
-    <>
-      <Flex align="end" mb="8">
-        <Box mr="4">{data.icon}</Box>
-        <Flex direction="column" className="flex-grow">
-          <Flex justify="space-between" align="end" className="mb-2">
-            <Heading size="4xl">{data.label}</Heading>
-            <Heading size="xl">{data.value}%</Heading>
-          </Flex>
-          <ProgressRoot
-            shape="full"
-            variant="subtle"
-            animated={true}
-            className="w-full"
-            value={data.value}
-          >
-            <ProgressBar />
-          </ProgressRoot>
+    <Flex direction="column" gap="4">
+      <Flex direction="column" gap="2">
+        <MotionHeading size="4xl" {...revealFromLeft}>
+          {title}
+        </MotionHeading>
+        <MotionBox h="1" bg="primary" rounded="sm" {...grow} />
+      </Flex>
+      <Flex w="full">
+        <MotionBox w="1/3" {...revealFromBottom} transition={{ delay: 0.2 }}>
+          <Text>{description}</Text>
+        </MotionBox>
+        <Flex wrap="wrap" w="2/3" gap="6" justify="end">
+          {skills.map((skill, i) => (
+            <MotionBox
+              key={skill.name}
+              {...revealFromBottom}
+              transition={{ delay: 0.4 + 0.1 * i }}
+            >
+              <Skill skill={skill} />
+            </MotionBox>
+          ))}
         </Flex>
       </Flex>
-      <Flex wrap="wrap" gap="3">
-        {data.skills.map((skill) => (
-          <Badge key={skill}>{skill}</Badge>
-        ))}
-      </Flex>
-    </>
+    </Flex>
   );
 };
 
--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -1,39 +1,56 @@
+import { transform } from "motion";
+
 export const revealFromBottom = {
   initial: {
     opacity: 0,
+    transform: "translateY(1rem)",
   },
   animate: {
-    transform: ["translateY(1rem)", "translateY(0)"],
-    opacity: [0, 1],
+    transform: "translateY(0)",
+    opacity: 1,
   },
 };
 
 export const revealFromTop = {
   initial: {
     opacity: 0,
+    transform: "translateY(-1rem)",
   },
   animate: {
-    transform: ["translateY(-1rem)", "translateY(0)"],
-    opacity: [0, 1],
+    transform: "translateY(0)",
+    opacity: 1,
   },
 };
 
 export const revealFromLeft = {
   initial: {
     opacity: 0,
+    transform: "translateX(-1rem)",
   },
   animate: {
-    transform: ["translateX(-1rem)", "translateX(0)"],
-    opacity: [0, 1],
+    transform: "translateX(0)",
+    opacity: 1,
   },
 };
 
 export const revealFromRight = {
   initial: {
     opacity: 0,
+    transform: "translateX(1rem)",
   },
   animate: {
-    transform: ["translateX(1rem)", "translateX(0)"],
-    opacity: [0, 1],
+    transform: "translateX(0)",
+    opacity: 1,
+  },
+};
+
+export const grow = {
+  initial: {
+    opacity: 0,
+    width: 0,
+  },
+  animate: {
+    opacity: 1,
+    width: "100%",
   },
 };
