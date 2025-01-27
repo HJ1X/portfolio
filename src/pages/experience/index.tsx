aefb076bd860741ae2c3bba6f7c2ffd27566d566
Himanshu Jaroli <54548986+HJ1X@users.noreply.github.com>
2025-01-27 18:44:11 +0530

    refactor: remove unwanted code

--- src/pages/experience/index.tsx
+++ src/pages/experience/index.tsx
@@ -1,8 +1,6 @@
-import { MotionHeading } from "@/components/ui/Motion";
-import { profileTimeline, experienceDetails } from "@/data/experienceData";
-import { revealFromBottom } from "@/utils/animation";
+import { experienceDetails, profileTimeline } from "@/data/experienceData";
 import { Box, Flex, Grid } from "@chakra-ui/react";
-import { useLayoutEffect, useRef, useState } from "react";
+import { useState } from "react";
 import ExperienceDetails from "./ExperienceDetails";
 import Timeline from "./Timeline";
 import ExperiencePageMobile from "./indexMobile";
@@ -14,16 +12,6 @@ const defaultProfile = {
 
 const ExperiencePage = () => {
   const [selectedProfile, setSelectedProfile] = useState(defaultProfile);
-  const [experienceTextTop, setExperienceTextTop] = useState(0);
-
-  const timelineRef = useRef<HTMLDivElement>(null);
-
-  useLayoutEffect(() => {
-    if (timelineRef.current) {
-      const { top } = timelineRef.current.getBoundingClientRect();
-      setExperienceTextTop(top);
-    }
-  }, []);
 
   const details = experienceDetails.find(
     (detail) =>
@@ -33,32 +21,9 @@ const ExperiencePage = () => {
 
   return (
     <>
-      {/* <Flex
-        position="absolute"
-        top={`calc(${experienceTextTop}px - 2.5rem)`}
-        right={{ base: "-2", xl: "-5", "2xl": "-20" }}
-      >
-        {"Experience".split("").map((char, i) => (
-          <MotionHeading
-            key={"#" + i}
-            fontSize="9rem"
-            color="gray"
-            {...revealFromBottom}
-            animate={{
-              ...revealFromBottom.animate,
-              opacity: 0.2,
-            }}
-            transition={{
-              delay: 0.05 * i,
-            }}
-          >
-            {char}
-          </MotionHeading>
-        ))}
-      </Flex> */}
       <Box h="full" hideBelow="lg">
         <Grid h="full" templateColumns="repeat(2, 1fr)" alignItems="center">
-          <Box width="full" mt="-10" ref={timelineRef}>
+          <Box width="full" mt="-10">
             <Timeline
               selectedProfile={selectedProfile}
               onProfileChange={(
